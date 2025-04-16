c2ea0d2217d7e9a6ecd8845e4b3a40f6ca6636e5
Dowha Kim <mail@dowha.kim>
2025-04-17 00:12:21 +0900

    fix: prevent layout shift by forcing vertical scrollbar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="overflow-y-scroll">
           <Main />
           <NextScript />
         </body>
